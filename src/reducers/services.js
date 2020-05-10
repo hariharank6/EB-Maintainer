68290b99a4489ff2d62b952956c83c19c70e0bcb
Vishnu K <vishnuaug98@gmail.com>
2020-05-10 16:11:54 +0530

    final rate calculated

--- src/reducers/services.js
+++ src/reducers/services.js
@@ -1,13 +1,30 @@
-const calculateRate = () => {
-    return calculatedRate;
+const calculateRate = ({units}, {meterReading}) => {
+    const subsidy = 150
+    const fixedPrice = [0, 20, 30, 50]
+    const usedUnits = meterReading - units
+    if (usedUnits <= 100){
+        const cost = 150 + fixedPrice[0]
+    }
+    else if (usedUnits <= 200){
+        const cost = (usedUnits * 1.5) + fixedPrice[1]
+    }
+    else if (usedUnits <= 500){
+        const cost = (100 * 1.5) + (100 * 2.0) + ((usedUnits - 200) * 3.0) + fixedPrice[2]
+    }
+    else{
+        const cost = (100 * 1.5) + (100 * 3.5) + (300 * 4.6) + ((usedUnits - 500) * 6.6) + fixedPrice[3]
+    }
+    calculateRate = cost - subsidy
+    return calculatedRate
 }
+
 export default (state=[], action) => {
     switch(action.type) {
         case "ADD_UNIT" :
             if(state && action.data && action.data.serviceNo && action.data.units && action.data.updatedDate) {
                 return state.map((service) => {
                     if(service.serviceNo == action.data.serviceNo) {                        
-                        const calculatedRate = calculateRate();
+                        const calculatedRate = calculateRate(action, state.billData)
                         
                         const pastUpdateData = {
                             units: service.units,
@@ -34,7 +51,7 @@ export default (state=[], action) => {
                 for(let stateIteration = 0; stateIteration < clonedState.length; stateIteration++) {
                     for(let actionIteration = 0; actionIteration < action.data.services.length; actionIteration++) {
                         if(clonedState[stateIteration].serviceNo == action.data.services[actionIteration].serviceNo) {
-                            const calculatedRate = calculateRate();
+                            const calculatedRate = calculateRate(action, state.billData)
 
                             const pastUpdateData = {
                                 units: clonedState[stateIteration].units,
@@ -47,7 +64,7 @@ export default (state=[], action) => {
                             clonedState[stateIteration].updatedDate = action.data.updatedDate
                             clonedState[stateIteration].pastUpdates = [...clonedState[stateIteration].pastUpdates, pastUpdateData]
                             updatedServicesCount++
-                            break;
+                            break
                         }
                     }
                 }
