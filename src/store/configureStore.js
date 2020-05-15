06209ddcb5287a02bba689a4f6604b84ade72856
Vishnu K <vishnuaug98@gmail.com>
2020-05-15 20:51:40 +0530

    data entry done.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -12,3 +12,13 @@ export default () => (
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 )
+
+// SERVICES:
+// i. 206003530
+// ii. 2070031691
+// iii. 2150031053
+// iv. 2150031054
+// v. 2150031055
+// vi. 2150031056
+// vii. 215003717
+// viii. 215003785
--- src/store/storeSync.js
+++ src/store/storeSync.js
@@ -8,7 +8,280 @@ export const storeToStorage = (updatedStoreData) => {
 }
 
 export const storageToStore = (store) => {
-    let storeData = {"services":[],suggestions:{}}
+    let storeData = {"services":[
+        {
+            "nickname": "name-3530",
+            "serviceNo": 206003530,
+            "entryDate": "22/04/2020",
+            "currencyCode": "₹",
+            "units": 110,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 0,
+                "rate": 0,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 0,
+                "rate": 0,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 87,
+              "meterReading": 100,
+              "billGeneratedDate": "19/04/2020",
+              "dueDate": "9/05/2020",
+              "billAmount": 0,
+              "paymentDate": "9/05/2020",
+              "isValidEntry": true
+            }
+        },
+        {
+            "nickname": "name-1691",
+            "serviceNo": 2070031691,
+            "entryDate": "27/03/2020",
+            "currencyCode": "₹",
+            "units": 13760,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 23,
+                "rate": 423,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 123,
+                "rate": 123,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 400,
+              "meterReading": 13750,
+              "billGeneratedDate": "24/03/2020",
+              "dueDate": "15/04/2020",
+              "billAmount": 830,
+              "paymentDate": "14/04/2020",
+              "isValidEntry": true
+            }
+        },
+        {
+            "nickname": "name-1053",
+            "serviceNo": 2150031053,
+            "entryDate": "15/04/2020",
+            "currencyCode": "₹",
+            "units": 770,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 23,
+                "rate": 423,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 123,
+                "rate": 123,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 550,
+              "meterReading": 760,
+              "billGeneratedDate": "12/04/2020",
+              "dueDate": "02/05/2020",
+              "billAmount": 2110,
+              "paymentDate": "02/05/2020",
+              "isValidEntry": true
+            }
+        },
+        {
+            "nickname": "name-1054",
+            "serviceNo": 2150031054,
+            "entryDate": "15/04/2020",
+            "currencyCode": "₹",
+            "units": 470,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 23,
+                "rate": 423,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 123,
+                "rate": 123,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 330,
+              "meterReading": 460,
+              "billGeneratedDate": "12/04/2020",
+              "dueDate": "02/05/2020",
+              "billAmount": 620,
+              "paymentDate": "02/05/2020",
+              "isValidEntry": true
+            }
+        },
+        {
+            "nickname": "name-1055",
+            "serviceNo": 2150031055,
+            "entryDate": "15/04/2020",
+            "currencyCode": "₹",
+            "units": 120,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 23,
+                "rate": 423,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 123,
+                "rate": 123,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 80,
+              "meterReading": 110,
+              "billGeneratedDate": "12/04/2020",
+              "dueDate": "02/05/2020",
+              "billAmount": 0,
+              "paymentDate": "02/05/2020",
+              "isValidEntry": true
+            }
+        },
+        {
+            "nickname": "name-1056",
+            "serviceNo": 2150031056,
+            "entryDate": "15/04/2020",
+            "currencyCode": "₹",
+            "units": 260,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 23,
+                "rate": 423,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 123,
+                "rate": 123,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 190,
+              "meterReading": 250,
+              "billGeneratedDate": "12/04/2020",
+              "dueDate": "02/05/2020",
+              "billAmount": 155,
+              "paymentDate": "02/05/2020",
+              "isValidEntry": true
+            }
+        },
+        {
+            "nickname": "name-717",
+            "serviceNo": 215003717,
+            "entryDate": "15/04/2020",
+            "currencyCode": "₹",
+            "units": 530,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 23,
+                "rate": 423,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 123,
+                "rate": 123,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 390,
+              "meterReading": 520,
+              "billGeneratedDate": "12/04/2020",
+              "dueDate": "02/05/2020",
+              "billAmount": 800,
+              "paymentDate": "02/05/2020",
+              "isValidEntry": true
+            }
+        },
+        {
+            "nickname": "name-785",
+            "serviceNo": 215003785,
+            "entryDate": "15/04/2020",
+            "currencyCode": "₹",
+            "units": 290,
+            "rate": 0,
+            "pastUpdates": [
+              {
+                "units": 0,
+                "rate": 0,
+                "entryDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": false
+              },
+              {
+                "units": 0,
+                "rate": 0,
+                "entryDate": "dd/mm/yy",
+                "paidDate": "dd/mm/yy",
+                "isValidEntry": true,
+                "isActualBill": true
+              }
+            ],
+            "billData": {
+              "unitsConsumed": 180,
+              "meterReading": 280,
+              "billGeneratedDate": "12/04/2020",
+              "dueDate": "02/05/2020",
+              "billAmount": 140,
+              "paymentDate": "02/05/2020",
+              "isValidEntry": true
+            }
+        }
+    ],suggestions:{}}
     try {
         const storageStoreData = localStorage.getItem("storeData")
         if(storageStoreData != null) {
