afcb923d4aa21dfb9e01fee16711298c9b72434d
Vishnu K <vishnuaug98@gmail.com>
2020-05-16 22:34:34 +0530

    debugging

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Redirect, Switch } from "react-router-dom"
 
 export default class Service extends React.Component {
     constructor(props) {
@@ -8,6 +9,7 @@ export default class Service extends React.Component {
         }
         console.log(this.state)
         this.toggleMoreInfo = this.toggleMoreInfo.bind(this)
+        this.setRedirect = this.setRedirect.bind(this)
     }
     toggleMoreInfo() {
         this.setState((prevState) => {
@@ -16,33 +18,38 @@ export default class Service extends React.Component {
         })
         // console.log(this.state)
     }
+    setRedirect(serviceNo) {
+        event.preventDefault()
+        console.log("Button clicked")
+        return (<Switch> <Redirect to='/addall' /> </Switch>)
+    }
     render() {
         return (
             <div>
-                {this.props && this.props.service && this.props.service.serviceNo && typeof this.props.service.units != "undefined" && typeof this.props.service.rate != "undefined" && this.props.service.nickname && this.props.service.entryDate &&
+                {this.props && this.props.service && this.props.service.serviceNo && this.props.service.nickname && typeof this.props.service.rate != "undefined" &&
                     <div className= {this.state.isOpened ? 'service service--expanded' : 'service'}>
                         <div className="service__titlebar" onClick={this.toggleMoreInfo}>
                             <div className="service__title">{this.props.service.nickname}</div>
-                            <div className="service__title-rate">{this.props.service.currencyCode + "." + this.props.service.rate}</div>
-                            <button className="service__add-button">Add</button>
+                            <div className="service__title-rate">{this.props.service.currencyCode + this.props.service.rate}</div>
+                            <button className="service__add-button" onClick={() => this.setRedirect(this.props.service.serviceNo)} >Add</button>
                         </div>
-                        {this.props.service.entryDate &&
+                        {this.props.service.entryDate && typeof this.props.service.units != "undefined" && typeof this.props.service.rate != "undefined" && this.props.service.billData && typeof this.props.service.billData.meterReading != "undefined" && 
                             <div className="service__unbilled-container">
                                 <div className="service__unbilled-title">Unbiled:</div>
                                 <div className="service__unbilled-content">
-                                    <div className="service__unbilled-entry-date">{this.props.service.entryDate}</div>
-                                    <div className="service__unbilled-units">{this.props.service.units}</div>
-                                    <div className="service__unbilled-amount">{this.props.service.currencyCode + "." + this.props.service.rate}</div>
+                                    <div className="service__unbilled-entry-date">Entry Date: {this.props.service.entryDate}</div>
+                                    <div className="service__unbilled-units">Units: {this.props.service.units - this.props.service.billData.meterReading}</div>
+                                    <div className="service__unbilled-amount">Amount: {this.props.service.currencyCode + this.props.service.rate}</div>
                                 </div>
                             </div>
                         }
-                        {(this.props.service.billData && this.props.service.billData.billAmount && this.props.service.billData.dueDate) &&
+                        {this.props.service.billData && typeof this.props.service.billData.billAmount != "undefined" && this.props.service.billData.dueDate &&
                             <div className="service__bill-container">
                                 <div className="service__bill-title">Bill:</div>
                                 <div className="service__bill-content">
-                                    <div className="service__due-date">{this.props.service.billData.dueDate}</div>
-                                    <div className="service__bill-amount">{this.props.service.currencyCode + "." + this.props.service.billData.billAmount}</div>
-                                    <div className="service__payment-status">{this.props.service.billData.paymentDate ? "Paid" : "Not Paid"}</div>
+                                    <div className="service__due-date">DueDate: {this.props.service.billData.dueDate}</div>
+                                    <div className="service__bill-amount">Bill Amount: {this.props.service.currencyCode + this.props.service.billData.billAmount}</div>
+                                    <div className="service__payment-status">Payment Status: {this.props.service.billData.paymentDate ? "Paid" : "Not Paid"}</div>
                                 </div>
                             </div>
                         }
