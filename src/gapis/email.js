13bfb6c54bf40f9c0a92c1d9eb2d13ae68e624a2
Vishnu K <vishnuaug98@gmail.com>
2020-06-15 23:47:22 +0530

    date corrections and data scrapping are done.

--- src/gapis/email.js
+++ src/gapis/email.js
@@ -1,4 +1,5 @@
 import appConfig from '../store/storeDataInitConfig'
+import { changeDateFormat } from "../components/dateComponent"
 
 const initClient = () => {
     gapi.load('client:auth2', initClientCbk)
@@ -18,6 +19,23 @@ const initClientCbk = () => {
             firebase.auth().signOut(); // Something went wrong, sign out
         }
     })
+}
+
+export const dataScrap = (vendor, message) => {
+    if (vendor == "amazonPay"){
+        const billAmount = parseFloat(message.split("Rs.")[1].split(" ")[0])
+        const consumerNumber = parseInt(message.split("Consumer Number ")[1].split(" ")[0].slice(2,))
+        const dueDate = changeDateFormat(message.split("due on ")[1].split(".")[0])
+        const messageData = {
+            billAmount,
+            consumerNumber,
+            dueDate
+        }
+        return messageData
+    }
+    else{
+        console.log("Invalid Vendor")
+    }
 }
 
 export {
